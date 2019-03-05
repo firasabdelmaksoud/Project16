40219e6d06ac70a100fcd7a6a58a26f0ccb7c8f9
farouk <mohamedfarouk.laroussi@esprit.tn>
2019-03-05 03:16:37 +0100

    help

--- help.sh
+++ help.sh
@@ -4,8 +4,8 @@ function help() {
   echo -e " -search : pour chercher et affciher les fichiers dont l'extension est mis en argument /n"
   echo -e " -week : pour chercher et affciher les fichiers modifieé depuis une semaine"
   echo -e " -before-week : pour chercher et affciher les fichiers modifieé avant une semaine"
-  echo -e " -search-tmp : pour chercher et affciher les fichiers qui finissent par ~ et les supprimer"
+  echo -e " -searchtmp : pour chercher et affciher les fichiers qui finissent par ~ et les supprimer"
   echo -e " -per : pour chercher et affciher le nombre de tout les fichiers et le nombre des fichier qui finisse par ~ et leur pourcentage"
-  echo -e " -s : sauvegarder le pourcentage des fichier qui finissent par ~ dans un fichier txt "
+  echo -e " -save : sauvegarder le pourcentage des fichier qui finissent par ~ dans un fichier txt "
 
 }
