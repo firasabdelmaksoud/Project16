f153435466d6e0dd5c4f9935da1459f321e6ee3b
farouk <mohamedfarouk.laroussi@esprit.tn>
2019-03-05 01:59:05 +0100

    logo

--- help.sh
+++ help.sh
@@ -2,4 +2,5 @@ function help() {
   #statements
   echo -e "\n bienvenu project16 \n \n \n "
   echo -e "\n -search : pour affciher les fichier dont l'extension"
+  echo -e "\n -search : pour affciher les fichier dont l'extension"
 }
