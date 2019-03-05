9126d3c474f12239fdd6ef770d2b6cdf3f6ee703
farouk <mohamedfarouk.laroussi@esprit.tn>
2019-03-05 03:00:09 +0100

    help

--- help.sh
+++ help.sh
@@ -1,6 +1,11 @@
 function help() {
   #statements
   echo -e "\n bienvenu project16 \n \n \n "
-  echo -e "\n -search : pour affciher les fichier dont l'extension"
-  echo -e "\n -search : pour affciher les fichier dont l'extension"
+  echo -e "\n -search : pour chercher et affciher les fichiers dont l'extension est mis en argument"
+  echo -e "\n -week : pour chercher et affciher les fichiers modifieé depuis une semaine"
+  echo -e "\n -before-week : pour chercher et affciher les fichiers modifieé avant une semaine"
+  echo -e "\n -search-tmp : pour chercher et affciher les fichiers qui finissent par ~ et les supprimer"
+  echo -e "\n -per : pour chercher et affciher le nombre de tout les fichiers et le nombre des fichier qui finisse par ~ et leur pourcentage"
+  echo -e "\n -s : sauvegarder le pourcentage des fichier qui finissent par ~ dans un fichier txt "
+
 }
